aea7e9995e0805ac23b5d78d4ceecc90075c657b
Son-OfAnton <admasterefe00@gmail.com>
2023-08-01 20:42:39 +0300

    added output comments

--- Week_1/Utilizing_NPM_packages/index.js
+++ Week_1/Utilizing_NPM_packages/index.js
@@ -4,21 +4,21 @@ const arr1 = [5, 4, 3, 2, 1]
 const arr2 = [1, 2, 3]
 
 const arrDiff = _.difference(arr1, arr2)
-console.log(arrDiff)
+console.log(arrDiff)  // Output: [ 5, 4 ]
 
 const sortedArr1 = _.sortBy(arr1)
-console.log(sortedArr1)
+console.log(sortedArr1)   // Output: [ 1, 2, 3, 4, 5 ]
 
 const maxArr1 = _.max(arr1)
-console.log(maxArr1)
+console.log(maxArr1)    // Output: 5
 
 const greaterThanThree = _.filter(arr1, (num) => num >= 3)
-console.log(greaterThanThree)
+console.log(greaterThanThree)   // Output: [ 5, 4, 3 ]
 
 const pairNeighbours = _.chunk(arr1, 2)
-console.log(pairNeighbours)
+console.log(pairNeighbours)   // Output: [ [ 5, 4 ], [ 3, 2 ], [ 1 ] ]
 
 const debouncedFunc = _.debounce((val) => console.log(val), 1000)
 debouncedFunc('a')
 debouncedFunc('b')
-debouncedFunc('c')
+debouncedFunc('c')    // Output: c
