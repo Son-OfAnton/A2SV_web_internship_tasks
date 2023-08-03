c6dae1c0faadb7ef4989e280b08db6562ab3bddb
Son-OfAnton <admasterefe00@gmail.com>
2023-08-03 14:08:26 +0300

    added Counter App

---
+++ Week_1/Counter/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import "./App.css"
+
+const App:React.FC = () => {
+    const [count, setCount] = React.useState(0)
+    
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
+    )
+}
+
+export default App;
