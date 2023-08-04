15e71e5c8632f4d163a0705520e80b569afab074
Son-OfAnton <admasterefe00@gmail.com>
2023-08-04 13:38:37 +0300

    implemented contact CRUD

---
+++ Week_1/Contact_management_app/src/routes/index.jsx
@@ -0,0 +1,10 @@
+export default function Index() {
+  return (
+    <div id="index-page">
+      <span role="img" aria-label="book emoji">
+        📝
+      </span>{" "}
+      Contact manager
+    </div>
+  );
+}
