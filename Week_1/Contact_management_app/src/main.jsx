cab0d8e2ce12d18ae08d719e9a21c7729f60b07d
Son-OfAnton <admasterefe00@gmail.com>
2023-08-05 00:29:27 +0300

    added searching

--- Week_1/Contact_management_app/src/main.jsx
+++ Week_1/Contact_management_app/src/main.jsx
@@ -11,7 +11,8 @@ import Root, {
 import ErrorPage from './error-page';
 
 import Contact, { 
-  loader as contactLoader 
+  loader as contactLoader,
+  action as contactAction, 
 } from './routes/contact';
 
 import EditContact, {
@@ -29,21 +30,27 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      {index: true, element: <Index />},
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index />},
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          },
+        ], 
       },
     ],
   }, 
